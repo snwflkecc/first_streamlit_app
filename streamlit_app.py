5ac66006bca6314a36f87fecc4cf99e61ecf934e
snwflkecc <134370892+snwflkecc@users.noreply.github.com>
2023-05-31 17:52:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,16 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-fruityviced_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityviced_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    fruityviced_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityviced_normalized)
 
 streamlit.stop()
 
